045cd7a2f4df3fa36cedd78dda3a202a308e6552
sabbirjiinat <sabbirshnt@gmail.com>
2023-05-02 10:21:27 +0600

    filtered recipe by category id

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 app.use(cors())
 const port = 5330;
 const chefCategory = require('./data/category.json')
+const recipes = require('./data/recipes.json')
 
 
 app.get('/', (req, res) => {
@@ -19,6 +20,12 @@ app.get('/chef/:id', (req, res) => {
   const id = req.params.id;
   const findChefById = chefCategory.find(chef => chef.id == id)
   res.send(findChefById)
+})
+
+app.get('/recipe/:id', (req, res) => {
+  const id = req.params.id;
+  const filterRecipeById = recipes.filter(recipe => recipe.category_id == id)
+  res.send(filterRecipeById)
 })
 
 
